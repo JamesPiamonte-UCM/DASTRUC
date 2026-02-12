7b1397c221ee31946caf046476ad2feddf0f062b
JamesPiamonte-UCM <jamespiamonte43@gmail.com>
2026-02-12 19:25:13 +0800

    Add files via upload

---
+++ StudentMain.java
@@ -0,0 +1,8 @@
+public class StudentMain {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("James Piamonte", 20, "BSIT-2");
+
+        student1.displayInfo();
+    }
+}
